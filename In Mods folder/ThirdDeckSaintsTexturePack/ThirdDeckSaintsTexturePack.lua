8ec5397946cca7d640d222d0c05db1fe00e603b3
KKingOfficial <102820358+KKingOfficial@users.noreply.github.com>
2024-12-28 15:05:31 -0500

    Main Mod Files
    Place the two folders within into your Mods Folder

---
+++ In Mods folder/ThirdDeckSaintsTexturePack/ThirdDeckSaintsTexturePack.lua
@@ -0,0 +1,26 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Third Deck Saints Texture Pack
+--- MOD_ID: ThirdDeckSaintsTexturePack
+--- MOD_AUTHOR: [ItsKKing]
+--- MOD_DESCRIPTION: Like any good Saint, you gotta look the part...
+--- DEPENDENCIES: [Steamodded>=1.0.0~ALPHA-0812d]
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+
+sendDebugMessage("Launching ThirdDeckSaints!", "ThirdDeckSaintsTexturePack")
+
+SMODS.Atlas{key = "Joker", path = "Jokers.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas{key = "centers", path = "FlatDarkEnhancers.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas{key = "cards_1", path = "FlatDarkDeck.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas{key = "ui_1", path = "FlatDarkUI.png", px = 18, py = 18, prefix_config = { key = false } }
+SMODS.Atlas{key = "balatro", path = "FlatDarkBalatro.png", px = 333, py = 216, prefix_config = { key = false } }
+SMODS.Atlas{key = "shop_sign", path = "ShopSignAnimation.png", px = 113, py = 57, prefix_config = { key = false }, atlas_table = 'ANIMATION_ATLAS', frames = 4}
+SMODS.Atlas{key = "collab_DTD_1", path = "collab_DTD_1.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas{key = "collab_EG_1", path = "collab_EG_1.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas{key = "collab_SV_1", path = "collab_SV_1.png", px = 71, py = 95, prefix_config = { key = false } }
+SMODS.Atlas{key = "collab_XR_1", path = "collab_XR_1.png", px = 71, py = 95, prefix_config = { key = false } }
+
+----------------------------------------------
+------------MOD CODE END----------------------
